51b08a23879c24455995b7c6a3daca443b5d6232
Buddy362 <danila.malyshko00@gmail.com>
2022-02-19 19:33:38 +0600

    Update №2
    New version of website

---
+++ js/mouseover.js
@@ -0,0 +1,12 @@
+function mouseOver() { document.getElementById("demo").style.color = "red";
+}
+function mouseOut() {
+document.getElementById("demo").style.color = "black";
+}
+function bigImg(x) {
+  x.style.transform = "scale(1.5)"
+}
+
+function normalImg(x) {
+  x.style.transform = "scale(1)"
+}
